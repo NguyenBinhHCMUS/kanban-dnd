a0d519b01d4f94a96673b38922a1a35fe44e35dc
CTLT-D3410-2\Administrator <ctv3.bcn1.e@ctgroupvietnam.com>
2022-07-15 16:06:22 +0700

    save stage

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import Kanban from "./components/kanban";
+
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ marginBottom: "20px" }}>Kanban UI</h1>
+      <Kanban />
+    </div>
+  );
+}
+
+export default App;
